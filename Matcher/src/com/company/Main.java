33a1d196d20a413f624de32c79f96d754f991246
maxut <anykey991@gmail.com>
2018-03-04 22:46:52 +0600

    release v2

--- Matcher/src/com/company/Main.java
+++ Matcher/src/com/company/Main.java
@@ -6,18 +6,30 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-	        boolean pattern = Pattern.matches("^[0-9]", "asdasd");
 
+	            /*  boolean pattern = Pattern.matches("^[0-9]", "asdasd");
 	        System.out.println("a".matches("^[1-9]"));
 
             String parsedPhone = parsePhone("+77076765656");
-            System.out.println(parsedPhone);
+            */
+            System.out.println(parsePhone("87076777878"));
+            System.out.println(parsePhone("7076777878"));
+            System.out.println(parsePhone("+77076777878"));
+
     }
 
     public static String parsePhone(String phone) {
-        if (phone.matches("^((\\+7)+([0-9]){10})$")) {
-            return "8" + phone.substring(2,12);
+        if (phone.matches("((\\+7)+([0-9]){10})$")) {
+            return "7" + phone.substring(2);
         }
+        else if (phone.matches("((8)+([0-9]){10})$")) {
+            return "7" + phone.substring(1);
+        }
+
+        else if (phone.matches("((^7)+([0-9]){9})$")) {
+            return "77" + phone.substring(1);
+        }
+
         return null;
     }
 }
